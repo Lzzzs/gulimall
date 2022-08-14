1c0ed1ba2b7c30762ace040968ea5dad4ce45ac1
liangzhensheng <1250564179@qq.com>
2022-08-14 11:43:52 +0800

    init info

---
+++ gulimall-ware/src/test/java/top/lzzzs/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lzzzs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
